ebad42f009c2df803bcacbc6825b5db56590a11b
Lucas Oliveira <just.lucasoliveira@gmail.com>
2018-07-08 17:04:53 -0300

    Add pagination to the loyalty points by customer page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ShopifyApp::AuthenticatedController
+  LIMIT = 10
+
   def index
     @points = shop.loyalty_points
+    @customers_count = ShopifyAPI::Customer.count
+
+    @pages_count = @customers_count / LIMIT
+    @current_page = (params[:page] || 1).to_i
+
     @customers = ShopifyAPI::Customer
-      .find(:all)
+      .find(:all, params: { limit: LIMIT, page: @current_page })
       .map { |customer| CustomerDecorator.new(customer, @points) }
       .sort_by { |customer| -customer.points_balance }
   end
